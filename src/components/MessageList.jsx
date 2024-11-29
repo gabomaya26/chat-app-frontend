1ec1cd3e81fb9bad97ea3e8c83cc3e0a73ea8ec5
gabomaya26 <fate142001@gmail.com>
2024-11-29 12:51:00 -0500

    upgrade messagelist.jsx - add url function open new page

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,5 +1,78 @@
 import Image from "next/image";
 
+function renderMessageWithLinks(message) {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  return message.split(" ").map((word, index) => {
+    if (urlRegex.test(word)) {
+      return (
+        <a
+          key={index}
+          href={word}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: 'underline', color: 'blue' }}
+        >
+          {word}
+        </a>
+      );
+    }
+    return word + " ";
+  });
+}
+
+export default function MessageList({ messages }) {
+  return (
+    <>
+      {messages?.map((message) => (
+        <div
+          key={message.id}
+          style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={
+              message.author.profile_picture
+                ? `https://chat-app-mu-blush.vercel.app${message.author.profile_picture}`
+                : "/img/no-photo.jpg"
+            }
+            width={50}
+            height={50}
+            style={{ borderRadius: "100%", objectFit: "cover" }}
+            quality={100}
+            priority={true}
+            unoptimized
+            alt="Perfil"
+          />
+          <span style={{ marginLeft: "10px" }}>
+            <b>{message.author.name}:</b> {renderMessageWithLinks(message.content)}
+          </span>
+        </div>
+      ))}
+    </>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** 
+import Image from "next/image";
+
 export default function MessageList({messages}) {
   return (
     <>
@@ -21,11 +94,11 @@ export default function MessageList({messages}) {
 
 
 //C:\Users\Sam-sephiol\Desktop\frontend\src\img\no-photo.jpg
-/**
+
  * 
  * <Image 
                 src={message.author.profile_picture != null ? `https://chat-app-mu-blush.vercel.app${message.author.profile_picture}` : "img/no-photo.jpg"}
                 width={50} height={50} style={{borderRadius: 100 + "%", objectFit:"cover" }} quality={100} priority={true} unoptimized alt="Perfil">
                 </Image>
  * 
- */
+**/
